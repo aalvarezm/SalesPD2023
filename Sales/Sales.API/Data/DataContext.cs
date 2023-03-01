4f2f27cc5cb635dd5740434a0969b88f3399ac7c
aalvarezm <andresalvarezm963@gmail.com>
2023-03-01 01:23:49 -0500

    Create database 01-03-2023

---
+++ Sales/Sales.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class DataContext : DbContext //Hereda
+    {
+        //Constructor de la clase datacontext
+        public DataContext(DbContextOptions<DataContext> options) : base(options) //se pasan los parámetros
+        {
+        }
+        public DbSet<Country> Countries { get; set; } // se plularizara
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique(); //
+        }
+    }
+}
---
+++ Sales/Sales.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        //Todas entidades tienen un Id
+        public int Id { get; set; }
+
+        //Manejo de DataAnnotations permite cambiarle el comportamiento a las entidades
+        // El 0 y el 1 se reemplazan por el orden de los dataannotations
+
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!; // Le indico que no será nulo
+
+    }
+}
